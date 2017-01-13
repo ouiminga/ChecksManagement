1f9c2df757cf96fd64726fdb911a1058248f968d
ouiminga <ouimaz@yahoo.fr>
2017-01-13 20:33:06 +0100

    test

--- ChecksManagement/src/bank/checks/controller/Check.java
+++ ChecksManagement/src/bank/checks/controller/Check.java
@@ -8,7 +8,7 @@ import java.util.Date;
 /**
  * @author ouiminga
  *
- */
+ */   
 public class Check {
 	public static int nbrInstanceOfChecks = 0;
 
@@ -17,7 +17,7 @@ public class Check {
 
 	long amount = 0;
 	long accountHolderAccountNumber = 0;
-int dd;
+	
 	String amountInLetters = "";
 	String nameOfRecipient = "";
 	String nameOfOrder = "";
